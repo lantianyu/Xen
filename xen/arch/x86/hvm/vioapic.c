13d49d2d55346c70efff1db50b3aeb95308a935c
Chao Gao <chao.gao@intel.com>
2017-03-17 19:27:15 +0800

    x86/vioapic: Hook interrupt delivery of vIOAPIC
    When irq remapping is enabled, IOAPIC Redirection Entry may be in remapping
    format. If that, generate an irq_remapping_request and call the common
    VIOMMU abstraction's callback to handle this interrupt request. Device
    model is responsible for checking the request's validity.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>
    Signed-off-by: Lan Tianyu <tianyu.lan@intel.com>

--- xen/arch/x86/hvm/vioapic.c
+++ xen/arch/x86/hvm/vioapic.c
@@ -30,6 +30,7 @@
 #include <xen/lib.h>
 #include <xen/errno.h>
 #include <xen/sched.h>
+#include <xen/viommu.h>
 #include <public/hvm/ioreq.h>
 #include <asm/hvm/io.h>
 #include <asm/hvm/vpic.h>
@@ -38,6 +39,8 @@
 #include <asm/current.h>
 #include <asm/event.h>
 #include <asm/io_apic.h>
+
+#include "../../../drivers/passthrough/vtd/vtd.h"
 
 /* HACK: Route IRQ0 only to VCPU0 to prevent time jumps. */
 #define IRQ0_SPECIAL_ROUTING 1
@@ -327,8 +330,19 @@ static void vioapic_deliver(struct hvm_vioapic *vioapic, unsigned int pin)
     struct vlapic *target;
     struct vcpu *v;
     unsigned int irq = vioapic->base_gsi + pin;
+    struct IO_APIC_route_remap_entry rte = { { vioapic->redirtbl[pin].bits } };
 
     ASSERT(spin_is_locked(&d->arch.hvm_domain.irq_lock));
+
+    if ( rte.format )
+    {
+        struct irq_remapping_request request;
+
+        irq_request_ioapic_fill(&request, vioapic->id, rte.val);
+        /* Currently, only viommu 0 is supported */
+        viommu_handle_irq_request(d, 0, &request);
+        return;
+    }
 
     HVM_DBG_LOG(DBG_LEVEL_IOAPIC,
                 "dest=%x dest_mode=%x delivery_mode=%x "
