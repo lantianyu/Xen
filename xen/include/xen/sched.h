a977e16c9cc7642f9d0150e9b5ba03a69da18986
Lan Tianyu <tianyu.lan@intel.com>
2017-03-17 19:27:01 +0800

    VIOMMU: Add vIOMMU helper functions to create, destroy and query capabilities
    This patch is to introduct an abstract layer for arch vIOMMU implementation
    to deal with requests from dom0. Arch vIOMMU code needs to provide callback
    to perform create, destroy and query capabilities operation.
    
    Signed-off-by: Lan Tianyu <tianyu.lan@intel.com>

--- xen/include/xen/sched.h
+++ xen/include/xen/sched.h
@@ -21,6 +21,7 @@
 #include <xen/perfc.h>
 #include <asm/atomic.h>
 #include <xen/wait.h>
+#include <xen/viommu.h>
 #include <public/xen.h>
 #include <public/domctl.h>
 #include <public/sysctl.h>
@@ -477,6 +478,7 @@ struct domain
     /* vNUMA topology accesses are protected by rwlock. */
     rwlock_t vnuma_rwlock;
     struct vnuma_info *vnuma;
+    struct viommu_info viommu;
 
     /* Common monitor options */
     struct {
